7a930512c9f26965dd8ca22d3f3420c3a7bbd954
Shatabdi <shatabdi.mohapatra@mindfiresolutions.com>
2015-04-01 19:39:51 +0530

    added validations

--- app/assets/javascripts/groups.js.coffee
+++ app/assets/javascripts/groups.js.coffee
@@ -1,3 +1,4 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+function IsEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,6 @@ class GroupsController < ApplicationController
 	def create
 		@emails = params[:email].split(",").map(&:strip)
 		@group = Group.new(group_params)
-		unless valid_email?(params[:emails])
-			render new
-			flash[:error] = "Your email format is not correct. Please check the format and enter again."
   end
 
 
@@ -17,18 +14,5 @@ class GroupsController < ApplicationController
 	def group_params
 		params.require(:group).permit(:name)
 	end
-
-	private
-  def valid_email?(emails)
-  	flag = true
-  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  	emails.each do |email|
-    	unless email =~ VALID_EMAIL_REGEX
-    		flag = false
-    		break
-    	end
-    end
-    return flag
-  end
 
 end
